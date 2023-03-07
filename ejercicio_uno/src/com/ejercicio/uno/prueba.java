c58e866a967933660610cb17e54a3d13322fa83a
Brian Essayan <110383805+BrianEssayan@users.noreply.github.com>
2023-03-07 12:06:10 +0100

    Add files via upload

---
+++ ejercicio_uno/src/com/ejercicio/uno/prueba.java
@@ -0,0 +1,37 @@
+package com.ejercicio.uno;
+
+public class prueba {
+    public static void main(String[] args) {
+        // 1) Numericos
+
+        // 1.1) Enteros
+        byte var1 = 4;
+        short var2 = 10;
+        int var3 = 30;
+        long var4 = 100;
+
+        // 1.2) Decimales
+        float var5 = 5.5f;
+        double var6 = 10.5d;
+
+        // 2) Booleano
+        boolean var7 = true;
+        boolean var8 = false;
+
+        // 3) Texto
+        char var9 = 'a';
+        String var10 = "Hola";
+
+        // Impresion de datos
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+        System.out.println(var6);
+        System.out.println(var7);
+        System.out.println(var8);
+        System.out.println(var9);
+        System.out.println(var10);
+    }
+}
