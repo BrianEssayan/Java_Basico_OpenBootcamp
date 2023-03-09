161f6f84dfbfb9561b97f057b3c9979223fef09e
Brian Essayan <110383805+BrianEssayan@users.noreply.github.com>
2023-03-09 09:20:55 +0100

    Add files via upload

---
+++ ejercicio_tres/src/com/example/ConcatenaStrings.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class ConcatenaStrings {
+    public static void main(String[] args) {
+
+        String[] nombres = {"Hola", "me", "llamo", "Brian"};
+
+        for (String nombre : nombres) {
+            System.out.print(nombre + " ");
+        }
+    }
+}
